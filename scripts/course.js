e8788b2ea450f43ebb74fcf2249bcc06d71f966a
Akmurza <akuklin@byupathway.edu>
2025-09-21 03:44:41 +0400

    corrected_issues

--- scripts/course.js
+++ scripts/course.js
@@ -6,7 +6,7 @@ const courses = [
         title: 'Introduction to Programming',
         credits: 2,
         certificate: 'Web and Computer Programming',
-        description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
+        description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages and use them to solve problems.',
         technology: [
             'Python'
         ],
@@ -18,7 +18,7 @@ const courses = [
         title: 'Web Fundamentals',
         credits: 2,
         certificate: 'Web and Computer Programming',
-        description: 'This course introduces students to careers in web site design and development. The course is hands- on with students actually participating in simple web designs and programming.It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
+        description: 'This course introduces students to careers in web site design and development.It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
         technology: [
             'HTML',
             'CSS'
@@ -31,7 +31,7 @@ const courses = [
         title: 'Programming with Functions',
         credits: 2,
         certificate: 'Web and Computer Programming',
-        description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
+        description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
         technology: [
             'Python'
         ],
@@ -43,7 +43,7 @@ const courses = [
         title: 'Dynamic Web Fundamentals',
         credits: 2,
         certificate: 'Web and Computer Programming',
-        description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
+        description: ' Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
         technology: [
             'HTML',
             'CSS',
@@ -57,7 +57,7 @@ const courses = [
         title: 'Frontend Web Development I',
         credits: 2,
         certificate: 'Web and Computer Programming',
-        description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
+        description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on accessibility, compliance, performance optimization.',
         technology: [
             'HTML',
             'CSS',
@@ -94,7 +94,7 @@ function displayCourses(coursesToShow) {
 document.addEventListener('DOMContentLoaded', function () {
     const filterButtons = document.querySelectorAll('.filter-btn');
 
-    // Display all courses initially
+    // Display all courses 
     displayCourses(courses);
 
     filterButtons.forEach(button => {
